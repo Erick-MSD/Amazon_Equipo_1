7c8322a3a9ee3fb398bdc21dc02ece8a0153f7ee
Erick-MSD <msantiago0608@gmail.com>
2025-11-01 15:44:43 -0600

    se arreglo algo

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './styles.css'
-
-function App(): JSX.Element {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
 
 const container = document.getElementById('root')!
 createRoot(container).render(
