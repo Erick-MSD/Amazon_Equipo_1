cba5e56b9061fc1d97eb964147f010aee287ab2a
Erick-MSD <msantiago0608@gmail.com>
2025-11-23 16:35:32 -0600

    feat: Update README.md for project structure and quick start instructions; add vercel.json for deployment configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Amazon_Equipo_1/',
+  base: process.env.VERCEL ? '/' : '/Amazon_Equipo_1/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
